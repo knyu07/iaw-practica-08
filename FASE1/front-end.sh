ed1df31bac8cad93e587a1cdb09232d54f72b20c
Pilar Ventura Fernández <43401525+knyu07@users.noreply.github.com>
2020-12-21 15:11:48 +0100

    Update front-end.sh

--- FASE1/front-end.sh
+++ FASE1/front-end.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -x
 
+#VARIABLES
+DB_NAME=wordpress_db
+DB_USER=wordpress_user
+DB_PASSWORD=wordpress_password
+IP_PRIVADA_FRONTEND=
+IP_MYSQL_SERVER=
+
 #Actualizamos
 apt update
 
@@ -10,11 +17,11 @@ apt install apache2 -y
 #Instalamos los módulos PHP
 apt install php libapache2-mod-php php-mysql -y
 
+#Copiamos el archivo info.php a /var/www/html
+cp /home/ubuntu/info.php /var/www/html
+
 #Reiniciamos el servicio de Apache
 systemctl restart apache2
-
-#Copiamos el archivo info.php a /var/www/html
-cp /var/www/html
 
 #Añadimos la URL del Wordpress
 cd /var/www/html
@@ -34,3 +41,53 @@ sed -i "s/database_name_here/$DB_NAME/" wp-config.php
 sed -i "s/username_here/$DB_USER/" wp-config.php
 sed -i "s/password_here/$DB_PASSWORD/" wp-config.php
 sed -i "s/localhost/$IP_MYSQL_SERVER/" wp-config.php
+
+#Habilitamos las variables WP_SITEURL y WP_HOME
+sed -i /DB_COLLATE/a define('WP_SITEURL'), 'http://$IP_PUBLICA_FRONTEND/wordpress');" /var/www/html/wordpress/wp_config.php
+sed -i /WP_SITEURL/a define('WP_HOME'), 'http://$IP_PUBLICA_FRONTEND');" /var/www/html/wordpress/wp_config.php
+
+#Copiar el archivo wordpress /index.php a /var/www/html
+
+cp /var/www/html/index.php /var/www/html
+
+#Editamos el archivo wordpress /index.php
+
+sed -i "s#wp-blog-header.php#wordpress/wp-blog-header.php#" /var/www/html/index.php
+
+#Habilitamos el módulo mod_rewrite de Apache
+
+a2enmod rewrite
+
+#Copiamos el archivo htaccess a /var/www/html
+cp /home/ubuntu/htaccess /var/www/html/.htaccess
+
+#Copiamos el archivo de configuración de Apache
+cp /home/ubuntu/iaw-practica-08/FASE1/000-default.conf /etc/apache2/sites-available
+
+#Reiniciamos Apache
+systemctl restart apache2 
+
+#Configuramos el archivo wp-config.php
+sed -i "/AUTH_KEY/d" /var/www/html/wordpress/wp-config.php
+sed -i "/SECURE_AUTH_KEY/d" /var/www/html/wordpress/wp-config.php
+sed -i "/LOGGED_IN_KEY/d" /var/www/html/wordpress/wp-config.php
+sed -i "/NONCE_KEY/d" /var/www/html/wordpress/wp-config.php
+sed -i "/AUTH_SALT/d" /var/www/html/wordpress/wp-config.php
+sed -i "/SECURE_AUTH_SALT/d" /var/www/html/wordpress/wp-config.php
+sed -i "/LOGGED_IN_SALT/d" /var/www/html/wordpress/wp-config.php
+sed -i "/NONCE_SALT/d" /var/www/html/wordpress/wp-config.php
+
+#Hacemos una llamada a la API de wordpress para obtener las security keys
+SECURITY_KEYS=$(curl https://api.wordpress.org/secret-key/1.1/salt/)
+
+#Reemplaza el carácter / por el carácter _
+SECURITY_KEYS=$(echo $SECURITY_KEY | tr / _)
+
+#Añadimos los security keys al archivo
+sed -i "/@-/a $SECURITY_KEYS" /var/www/html/wordpress/wp-config.php
+
+# Eliminamos el archivo index.html del /var/www/html
+rm -f /var/www/html/index.html
+
+# Cambiamos el propietario y el grupo al directorio /var/www/html
+chown www-data:www-data /var/www/html/ -R
