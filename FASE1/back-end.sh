1dde6c7042e5b676b768ec035b854257c3661bc6
Pilar Ventura Fernández <43401525+knyu07@users.noreply.github.com>
2020-12-21 15:10:50 +0100

    Update back-end.sh

--- FASE1/back-end.sh
+++ FASE1/back-end.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 set -x
 
-#Actualizamos
-apt update
-
 #VARIABLES
 DB_NAME=wordpress_db
 DB_USER=wordpress_user
 DB_PASSWORD=wordpress_password
-IP_PRIVADA_FRONTEND=172.31.94.203
-IP_MYSQL_SERVER=172.31.94.203
+IP_PRIVADA_FRONTEND="%"
+IP_PRIVADA_BACK=
+
+#Actualizamos
+apt update
 
 #Instalamos MySQL Server
 apt install mysql-server -y
 
-#Instalamos los módulos PHP
-apt install php libapache2-mod-php php-mysql -y
-
 #Creamos la base de datos para wordpress
 mysql -u root <<< "DROP DATABASE IF EXISTS $DB_NAME;"
 mysql -u root <<< "CREATE DATABASE $DB_NAME;"
-mysql -u root <<< "CREATE USER $DB_USER@$IP_PRIVADA_FRONTEND IDENTIFIED BY '$DB_PASSWORD;'"
-mysql -u root <<< "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@$IP_PRIVADA_FRONTEND;"
+mysql -u root <<< "CREATE USER $DB_USER@'$IP_PRIVADA_FRONTEND' IDENTIFIED BY '$DB_PASSWORD;'"
+mysql -u root <<< "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'$IP_PRIVADA_FRONTEND';"
 mysql -u root <<< "FLUSH PRIVILEGES;"
+
+# Modificamos el valor de bind-address para permitir conexiones remotas
+sed -i "s/127.0.0.1/$IP_PRIVADA_BACK/" /etc/mysql/mysqld.conf.d/mysqld.conf
+
+# Reiniciamos mysql
+systemctl restart mysql
