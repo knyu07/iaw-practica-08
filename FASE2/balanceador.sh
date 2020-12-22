439a30e841b4390ee063ff359f458fcbfaf46df9
Pilar Ventura Fernández <43401525+knyu07@users.noreply.github.com>
2020-12-22 15:20:11 +0100

    Update balanceador.sh

--- FASE2/balanceador.sh
+++ FASE2/balanceador.sh
@@ -5,8 +5,8 @@ set -x
 # Variables de configuración
 #-----------------------------------
 
-IP_FRONT_1=54.83.107.89
-IP_FRONT_2=54.236.166.148
+IP_FRONT_1=
+IP_FRONT_2=
 
 # Actualizamos la lista de paquetes
 apt update
