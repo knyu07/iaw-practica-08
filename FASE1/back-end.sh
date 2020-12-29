25c2aaab83cf535e5e0c70009483eebf3093b187
Pilar Ventura Fernández <43401525+knyu07@users.noreply.github.com>
2020-12-29 18:02:10 +0100

    Update back-end.sh

--- FASE1/back-end.sh
+++ FASE1/back-end.sh
@@ -17,7 +17,7 @@ apt install mysql-server -y
 #Creamos la base de datos para wordpress
 mysql -u root <<< "DROP DATABASE IF EXISTS $DB_NAME;"
 mysql -u root <<< "CREATE DATABASE $DB_NAME;"
-mysql -u root <<< "CREATE USER $DB_USER@'$IP_PRIVADA_FRONTEND' IDENTIFIED BY '$DB_PASSWORD;'"
+mysql -u root <<< "CREATE USER $DB_USER@'$IP_PRIVADA_FRONTEND' IDENTIFIED BY '$DB_PASSWORD';"
 mysql -u root <<< "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'$IP_PRIVADA_FRONTEND';"
 mysql -u root <<< "FLUSH PRIVILEGES;"
 
