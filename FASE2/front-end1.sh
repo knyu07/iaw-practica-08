78f07b4b926dafcedb9315a88b53b174c7930e79
Pilar Ventura Fernández <43401525+knyu07@users.noreply.github.com>
2021-01-02 18:06:01 +0100

    Update front-end1.sh

--- FASE2/front-end1.sh
+++ FASE2/front-end1.sh
@@ -44,12 +44,12 @@ sed -i "s/password_here/$DB_PASSWORD/" wp-config.php
 sed -i "s/localhost/$IP_PRIVADA_MYSQL_SERVER/" wp-config.php
 
 #Habilitamos las variables WP_SITEURL y WP_HOME
-sed -i /DB_COLLATE/a define('WP_SITEURL'), 'http://$IP_PUBLICA_BALANCEADOR/wordpress');" /var/www/html/wordpress/wp-config.php
-sed -i /WP_SITEURL/a define('WP_HOME'), 'http://$IP_PUBLICA_BALANCEADOR');" /var/www/html/wordpress/wp-config.php
+sed -i /DB_COLLATE/a define('WP_SITEURL', 'http://$IP_PUBLICA_BALANCEADOR/wordpress');" /var/www/html/wordpress/wp-config.php
+sed -i /WP_SITEURL/a define('WP_HOME', 'http://$IP_PUBLICA_BALANCEADOR');" /var/www/html/wordpress/wp-config.php
 
 #Copiar el archivo wordpress /index.php a /var/www/html
 
-cp /var/www/html/index.php /var/www/html
+cp /var/www/html/wordpress/index.php /var/www/html
 
 #Editamos el archivo wordpress /index.php
 
@@ -60,10 +60,10 @@ sed -i "s#wp-blog-header.php#wordpress/wp-blog-header.php#" /var/www/html/index.
 a2enmod rewrite
 
 #Copiamos el archivo htaccess a /var/www/html
-cp /home/ubuntu/htaccess /var/www/html/.htaccess
+cp /home/ubuntu/iaw-practica-8/FASE2/htaccess /var/www/html/.htaccess
 
 #Copiamos el archivo de configuración de Apache
-cp /home/ubuntu/iaw-practica-08/FASE1/000-default.conf /etc/apache2/sites-available
+cp /home/ubuntu/iaw-practica-08/FASE2/000-default.conf /etc/apache2/sites-available
 
 #Reiniciamos Apache
 systemctl restart apache2 
