dc92ca75768404c762e135551cfa88fc9a65b74d
Pilar Ventura Fernández <43401525+knyu07@users.noreply.github.com>
2021-01-05 13:53:38 +0100

    Update front-end.sh

--- FASE1/front-end.sh
+++ FASE1/front-end.sh
@@ -60,10 +60,12 @@ sed -i "s#wp-blog-header.php#wordpress/wp-blog-header.php#" /var/www/html/index.
 a2enmod rewrite
 
 #Copiamos el archivo htaccess a /var/www/html
-mv /home/ubuntu/iaw-practica-8/FASE1/htaccess /var/www/html/.htaccess
+cd iaw-practica-8/FASE1
+mv /htaccess /var/www/html/.htaccess
 
 #Copiamos el archivo de configuración de Apache
-cp /home/ubuntu/iaw-practica-08/FASE1/000-default.conf /etc/apache2/sites-available
+cd iaw-practica-8/FASE1
+cp /000-default.conf /etc/apache2/sites-available/000-default.conf
 
 #Reiniciamos Apache
 systemctl restart apache2 
