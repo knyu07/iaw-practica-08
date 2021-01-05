b58e6f505ab2ada1610ed60f0dfe1aa454790b91
Pilar Ventura Fernández <43401525+knyu07@users.noreply.github.com>
2021-01-05 13:48:25 +0100

    Update front-end1.sh

--- FASE2/front-end1.sh
+++ FASE2/front-end1.sh
@@ -44,8 +44,8 @@ sed -i "s/password_here/$DB_PASSWORD/" wp-config.php
 sed -i "s/localhost/$IP_PRIVADA_MYSQL_SERVER/" wp-config.php
 
 #Habilitamos las variables WP_SITEURL y WP_HOME
-sed -i /DB_COLLATE/a define('WP_SITEURL', 'http://$IP_PUBLICA_BALANCEADOR/wordpress');" /var/www/html/wordpress/wp-config.php
-sed -i /WP_SITEURL/a define('WP_HOME', 'http://$IP_PUBLICA_BALANCEADOR');" /var/www/html/wordpress/wp-config.php
+sed -i "/DB_COLLATE/a define('WP_SITEURL', 'http://$IP_PUBLICA_BALANCEADOR/wordpress');" /var/www/html/wordpress/wp-config.php
+sed -i "/WP_SITEURL/a define('WP_HOME', 'http://$IP_PUBLICA_BALANCEADOR');" /var/www/html/wordpress/wp-config.php
 
 #Copiar el archivo wordpress /index.php a /var/www/html
 
@@ -60,6 +60,7 @@ sed -i "s#wp-blog-header.php#wordpress/wp-blog-header.php#" /var/www/html/index.
 a2enmod rewrite
 
 #Copiamos el archivo htaccess a /var/www/html
+cd iaw-practica-8/FASE2/htaccess /var/www/html/.htaccess
 mv /home/ubuntu/iaw-practica-8/FASE2/htaccess /var/www/html/.htaccess
 
 #Copiamos el archivo de configuración de Apache
@@ -82,7 +83,7 @@ sed -i "/NONCE_SALT/d" /var/www/html/wordpress/wp-config.php
 SECURITY_KEYS=$(curl https://api.wordpress.org/secret-key/1.1/salt/)
 
 #Reemplaza el carácter / por el carácter _
-SECURITY_KEYS=$(echo $SECURITY_KEY | tr / _)
+SECURITY_KEYS=$(echo $SECURITY_KEYS | tr / _)
 
 #Añadimos los security keys al archivo
 sed -i "/@-/a $SECURITY_KEYS" /var/www/html/wordpress/wp-config.php
