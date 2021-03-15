eb0beb70ce68e2f5773fb1317961ac0d199fa7d9
Pilar Ventura Fernández <43401525+knyu07@users.noreply.github.com>
2021-03-15 14:13:13 +0100

    Update front-end2.sh

--- FASE2/front-end2.sh
+++ FASE2/front-end2.sh
@@ -2,11 +2,6 @@
 set -x
 
 #VARIABLES
-DB_NAME=wordpress_db
-DB_USER=wordpress_user
-DB_PASSWORD=wordpress_password
-IP_PUBLICA_BALANCEADOR=
-IP_PRIVADA_MYSQL_SERVER=
 IP_PRIVADA_FRONTEND_NFS_SERVER=
 
 #Actualizamos
@@ -23,76 +18,6 @@ cp /home/ubuntu/info.php /var/www/html
 
 #Reiniciamos el servicio de Apache
 systemctl restart apache2
-
-#Añadimos la URL del Wordpress
-cd /var/www/html
-wget http://wordpress.org/latest.tar.gz
-
-#Descomprimimos el .tar.gz
-tar -xzvf latest.tar.gz
-
-#Eliminamos el tar.gz
-rm latest.tar.gz
-
-#Configuramos  el archivo de configuración de Wordpress
-cd /var/www/html/wordpress
-mv wp-config-sample.php wp-config.php
-
-sed -i "s/database_name_here/$DB_NAME/" wp-config.php
-sed -i "s/username_here/$DB_USER/" wp-config.php
-sed -i "s/password_here/$DB_PASSWORD/" wp-config.php
-sed -i "s/localhost/$IP_PRIVADA_MYSQL_SERVER/" wp-config.php
-
-#Habilitamos las variables WP_SITEURL y WP_HOME
-sed -i "/DB_COLLATE/a define('WP_SITEURL', 'http://$IP_PUBLICA_BALANCEADOR/wordpress');" /var/www/html/wordpress/wp-config.php
-sed -i "/WP_SITEURL/a define('WP_HOME', 'http://$IP_PUBLICA_BALANCEADOR');" /var/www/html/wordpress/wp-config.php
-
-#Copiar el archivo wordpress /index.php a /var/www/html
-
-cp /var/www/html/wordpress/index.php /var/www/html
-
-#Editamos el archivo wordpress /index.php
-
-sed -i "s#wp-blog-header.php#wordpress/wp-blog-header.php#" /var/www/html/index.php
-
-#Habilitamos el módulo mod_rewrite de Apache
-
-a2enmod rewrite
-
-#Copiamos el archivo htaccess a /var/www/html
-cd iaw-practica-8/FASE2
-cp htaccess /var/www/html/.htaccess
-
-#Copiamos el archivo de configuración de Apache
-cp 000-default.conf /etc/apache2/sites-available/000-default.conf
-
-#Reiniciamos Apache
-systemctl restart apache2 
-
-#Configuramos el archivo wp-config.php
-sed -i "/AUTH_KEY/d" /var/www/html/wordpress/wp-config.php
-sed -i "/SECURE_AUTH_KEY/d" /var/www/html/wordpress/wp-config.php
-sed -i "/LOGGED_IN_KEY/d" /var/www/html/wordpress/wp-config.php
-sed -i "/NONCE_KEY/d" /var/www/html/wordpress/wp-config.php
-sed -i "/AUTH_SALT/d" /var/www/html/wordpress/wp-config.php
-sed -i "/SECURE_AUTH_SALT/d" /var/www/html/wordpress/wp-config.php
-sed -i "/LOGGED_IN_SALT/d" /var/www/html/wordpress/wp-config.php
-sed -i "/NONCE_SALT/d" /var/www/html/wordpress/wp-config.php
-
-#Hacemos una llamada a la API de wordpress para obtener las security keys
-SECURITY_KEYS=$(curl https://api.wordpress.org/secret-key/1.1/salt/)
-
-#Reemplaza el carácter / por el carácter _
-SECURITY_KEYS=$(echo $SECURITY_KEYS | tr / _)
-
-#Añadimos los security keys al archivo
-sed -i "/@-/a $SECURITY_KEYS" /var/www/html/wordpress/wp-config.php
-
-# Eliminamos el archivo index.html del /var/www/html
-rm -f /var/www/html/index.html
-
-# Cambiamos el propietario y el grupo al directorio /var/www/html
-chown www-data:www-data /var/www/html/ -R
 
 #Instalamos el cliente NFS
 apt install nfs-common
